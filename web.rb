ad2de3b5b39a21223a6c36f7defdb24adc7b6099
Erika Pauwels <erika.pauwels@tenforce.com>
2015-05-25 16:32:40 +0200

    Make registration service compliant to JSON API (commit e97547b)

--- web.rb
+++ web.rb
@@ -20,30 +20,36 @@ MU = RDF::Vocabulary.new('http://mu.semte.ch/vocabulary/')
 
 
 ###
-# POST /register
+# POST /accounts
 #
-# Body    { "name": "John Doe", "nickname": "john_doe", "password": "secret", "passwordConfirmation": "secret" }
+# Body    {"data":{"type":"accounts","attributes":{"name":"John Doe","nickname":"john_doe","password":"secret","passwordConfirmation":"secret"}}}
 # Returns 200 on successful registration
 #         400 if body is invalid
 ###
-post '/register' do
+post '/accounts' do
   content_type :json
 
   request.body.rewind 
-  data = JSON.parse request.body.read
-
+  body = JSON.parse(request.body.read)
+  data = body['data']
+  attributes = data['attributes']
 
   ###
-  # Validate body
+  # Validate request
   ###
 
-  halt 400, { errors: { title: 'Nickname might not be blank' } }.to_json if data['nickname'].nil? or data['nickname'].empty?
+  rewrite_url = request.env['HTTP_X_REWRITE_URL']
+  error('X-Rewrite-URL header is missing') if rewrite_url.nil?
+
+  error('Incorrect type. Type must be accounts') if data['type'] != 'accounts'
+
+  error('Nickname might not be blank') if attributes['nickname'].nil? or attributes['nickname'].empty?
   
-  result = select_account_by_nickname(data['nickname'])
-  halt 400, { errors: { title: 'Nickname already exists' } }.to_json if not result.empty?
+  result = select_account_by_nickname(attributes['nickname'])
+  error('Nickname already exists') if not result.empty?
 
-  halt 400, { errors: { title: 'Password might not be blank' } }.to_json if data['password'].nil? or data['password'].empty?
-  halt 400, { errors: { title: 'Password and password confirmation do not match' } }.to_json if data['password'] != data['passwordConfirmation']
+  error('Password might not be blank') if attributes['password'].nil? or attributes['password'].empty?
+  error('Password and password confirmation do not match') if attributes['password'] != attributes['passwordConfirmation']
 
 
   ###
@@ -51,54 +57,53 @@ post '/register' do
   ###
 
   account_salt = SecureRandom.hex
-  hashed_password = (Digest::MD5.new << data['password'] + settings.salt + account_salt).hexdigest
+  hashed_password = (Digest::MD5.new << attributes['password'] + settings.salt + account_salt).hexdigest
 
 
   ###
   # Create user and account
   ###
 
-  user_uri = settings.graph + "/users/" + SecureRandom.uuid
-  account_uri = settings.graph + "/accounts/" + SecureRandom.uuid
-  create_user_and_account(user_uri, data['name'], account_uri, data['nickname'], hashed_password, account_salt)
+  user_id = SecureRandom.uuid
+  account_id = SecureRandom.uuid
+  create_user_and_account(user_id, attributes['name'], account_id, attributes['nickname'], hashed_password, account_salt)
 
 
   status 201
   {
-   name: data['name'],
-   nickname: data['nickname'].downcase
+    data: {
+      type: 'accounts',
+      id: account_id,
+      attributes: {
+        name: attributes['name'],
+        nickname: attributes['nickname'].downcase
+      },
+      links: {
+        self: rewrite_url.chomp('/') + '/' + account_id
+      }
+   }
   }.to_json
 
 end
 
 
 ###
-# POST /unregister
+# DELETE /accounts/:id
 #
-# Body    { "nickname": "john_doe", "password": "secret" }
 # Returns 200 on successful unregistration
-#         400 if nickname or password is invalid
+#         404 if account with given id doesn't exist
 ###
-post '/unregister' do
+delete '/accounts/:id' do
   content_type :json
 
-  request.body.rewind 
-  data = JSON.parse request.body.read
-
 
   ###
-  # Validate login
+  # Validate account id
   ###
 
-  result = select_account_credentials_by_nickname(data['nickname'])
-  halt 400 if result.empty? # no account with given nickname
-
+  result = select_account_by_id(params['id'], false)
+  halt 404 if result.empty? # no account with given identifier
   account = result.first
-  db_password = account[:password].to_s
-  password = Digest::MD5.new << data['password'] + settings.salt + account[:salt].to_s
-
-  halt 400 unless db_password == password.hexdigest # incorrect password given
-
 
   ### 
   # Update account status
@@ -106,53 +111,59 @@ post '/unregister' do
 
   update_account_status(account[:uri], MU['account/status/inactive'])
 
-
-  status 200
+  status 204
 
 end
 
 
 
 ###
-# POST /changePassword
+# PATCH /accounts/:id
 #
-# Body    { "nickname": "john_doe", "oldPassword": "secret", "newPassword": "anothersecret", "newPasswordConfirmation": "anothersecret" }
-# Returns 200 on successful change of password
-#         400 if nickname or old password is invalid
-#         400 if new passwords don't match
+# Body    {"data":{"type":"accounts","id":"1","attributes":{"nickname":"john_doe","password":"newsecret"}}}
+# Returns 200 on successful update
+#         400 if account is inactive
+#         400 if nickname is not unique
 ###
-post '/changePassword' do
+patch '/accounts/:id' do
   content_type :json
 
   request.body.rewind 
-  data = JSON.parse request.body.read
+  body = JSON.parse request.body.read
+  data = body['data']
+  attributes = data['attributes']
 
 
   ###
   # Validate body
   ###
+  error('Incorrect type. Type must be accounts') if data['type'] != 'accounts'
+  error('Incorrect id. Id does not match the request URL.') if data['id'] != params['id']
 
-  halt 400, { errors: { title: 'New password and password confirmation do not match' } }.to_json if data['newPassword'] != data['newPasswordConfirmation']
-
-  result = select_account_credentials_by_nickname(data['nickname'], MU['account/status/active'])
-  halt 400 if result.empty? # no active account with given nickname
-
+  result = select_account_by_id(data['id'])
+  halt 404 if result.empty? # no active account with given id
   account = result.first
-  db_password = account[:password].to_s
-  password = Digest::MD5.new << data['oldPassword'] + settings.salt + account[:salt].to_s
 
-  halt 400 unless db_password == password.hexdigest # incorrect old password given
+  unless attributes['nickname'].nil?
+    result = select_account_by_nickname(attributes['nickname']) 
+    error('Nickname already exists') if not result.empty? and result.first[:uri] != account[:uri] # another account with the given nickname already exists
+  end
+
+  error('User name cannot be updated') if not attributes['name'].nil?
 
 
   ###
   # Hash and store new user password with custom salt
   ###
 
-  account_salt = SecureRandom.hex
-  hashed_password = (Digest::MD5.new << data['newPassword'] + settings.salt + account_salt).hexdigest
-  update_account_credentials(account[:uri], hashed_password, account_salt)
+  unless attributes['password'].nil?
+    account_salt = SecureRandom.hex
+    hashed_password = (Digest::MD5.new << attributes['password'] + settings.salt + account_salt).hexdigest
+    update_account(account[:uri], hashed_password, account_salt, attributes['nickname'])
+  end
 
-  status 200
+
+  status 204
 end
 
 
@@ -162,17 +173,22 @@ end
 
 helpers do
 
-  def create_user_and_account(user_uri, name, account_uri, nickname, hashed_password, account_salt)
+  def create_user_and_account(user_id, name, account_id, nickname, hashed_password, account_salt)
+    user_uri = settings.graph + "/users/" + user_id 
+    account_uri = settings.graph + "/accounts/" + account_id 
     now = DateTime.now.xmlschema
+
     query =  " INSERT DATA {"
     query += "   GRAPH <#{settings.graph}> {"
     query += "     <#{user_uri}> a <#{FOAF.Person}> ;"
     query += "                   <#{FOAF.name}> \"#{name}\" ;"
     query += "                   <#{FOAF.account}> <#{account_uri}> ;"
+    query += "                   <#{MU.uuid}> \"#{user_id}\" ;"
     query += "                   <#{DC.created}> \"#{now}\"^^xsd:dateTime ;"
     query += "                   <#{DC.modified}> \"#{now}\"^^xsd:dateTime ."
     query += "     <#{account_uri}> a <#{FOAF.OnlineAccount}> ;"
     query += "                      <#{FOAF.accountName}> \"#{nickname.downcase}\" ;"
+    query += "                      <#{MU.uuid}> \"#{account_id}\" ;"
     query += "                      <#{MU['account/password']}> \"#{hashed_password}\" ;"
     query += "                      <#{MU['account/salt']}> \"#{account_salt}\" ;"
     query += "                      <#{MU['account/status']}> <#{MU['account/status/active']}> ;"
@@ -184,35 +200,45 @@ helpers do
   end
 
   def select_account_by_nickname(nickname)
-    query =  " SELECT ?account FROM <#{settings.graph}> WHERE {"
-    query += "   ?account a <#{FOAF.OnlineAccount}> ;"
-    query += "              <#{FOAF.accountName}> '#{nickname.downcase}' . "
+    query =  " SELECT ?uri FROM <#{settings.graph}> WHERE {"
+    query += "   ?uri a <#{FOAF.OnlineAccount}> ;"
+    query += "          <#{FOAF.accountName}> '#{nickname.downcase}' . "
     query += " }"
     settings.sparql_client.query query  
   end
 
-  def select_account_credentials_by_nickname(nickname, status_uri = nil)
-    query =  " SELECT ?uri ?password ?salt FROM <#{settings.graph}> WHERE {"
+  def select_account_by_id(id, filter_active = true)
+    query =  " SELECT ?uri FROM <#{settings.graph}> WHERE {"
     query += "   ?uri a <#{FOAF.OnlineAccount}> ;"
-    query += "        <#{FOAF.accountName}> '#{nickname.downcase}' ;"
-    query += "        <#{MU['account/status']}> <#{status_uri}> ;" unless status_uri.nil?
-    query += "        <#{MU['account/password']}> ?password ;"
-    query += "        <#{MU['account/salt']}> ?salt ."
+    query += "          <#{MU['account/status']}> <#{MU['account/status/active']}> ;" if filter_active
+    query += "          <#{MU.uuid}> '#{id}' . "
     query += " }"
-    settings.sparql_client.query query
+    settings.sparql_client.query query  
   end
 
-  def update_account_credentials(account_uri, hashed_password, account_salt)
+  def update_account(account_uri, hashed_password, account_salt, nickname)
     # Delete old password and salt
     query =  " WITH <#{settings.graph}> "
     query += " DELETE {"
-    query += "   <#{account_uri}> <#{MU['account/password']}> ?password ;"
-    query += "                    <#{MU['account/salt']}> ?salt ;"
+    query += "   <#{account_uri}> "
+    unless hashed_password.nil? or account_salt.nil?
+      query += "                  <#{MU['account/password']}> ?password ;"
+      query += "                  <#{MU['account/salt']}> ?salt ;"
+    end
+    unless nickname.nil?
+      query += "                  <#{FOAF.accountName}> ?nickname ;"
+    end
     query += "                    <#{DC.modified}> ?modified ."
     query += " }"
     query += " WHERE {"
-    query += "   <#{account_uri}> <#{MU['account/password']}> ?password ;"
-    query += "                    <#{MU['account/salt']}> ?salt ;"
+    query += "   <#{account_uri}> "
+    unless hashed_password.nil? or account_salt.nil?
+      query += "                  <#{MU['account/password']}> ?password ;"
+      query += "                  <#{MU['account/salt']}> ?salt ;"
+    end
+    unless nickname.nil?
+      query += "                  <#{FOAF.accountName}> ?nickname ;"
+    end
     query += "                    <#{DC.modified}> ?modified ."
     query += " }"
     settings.sparql_client.update(query)
@@ -221,8 +247,14 @@ helpers do
     now = DateTime.now.xmlschema
     query =  " INSERT DATA {"
     query += "   GRAPH <#{settings.graph}> {"
-    query += "     <#{account_uri}> <#{MU['account/password']}> \"#{hashed_password}\" ;"
-    query += "                      <#{MU['account/salt']}> \"#{account_salt}\" ;"
+    query += "     <#{account_uri}> "
+    unless hashed_password.nil? or account_salt.nil?
+      query += "                    <#{MU['account/password']}> \"#{hashed_password}\" ;"
+      query += "                    <#{MU['account/salt']}> \"#{account_salt}\" ;"
+    end
+    unless nickname.nil?
+      query += "                    <#{FOAF.accountName}> \"#{nickname.downcase}\" ;"
+    end
     query += "                      <#{DC.modified}> \"#{now}\"^^xsd:dateTime ."
     query += "   }"
     query += " }"
@@ -252,4 +284,9 @@ helpers do
     query += " }"
     settings.sparql_client.update(query)
   end
+
+  def error(title, status = 400)
+    halt status, { errors: [{ title: title }] }.to_json
+  end
+
 end
