3c22a1310adf0708ba84b9926425117cf333b902
Erika Pauwels <erika.pauwels@gmail.com>
2025-09-05 19:52:45 +0200

    Make generate-account script non-interactive

--- scripts/generate-account/run.sh
+++ scripts/generate-account/run.sh
@@ -3,63 +3,74 @@ require 'bundler/inline'
 require 'securerandom'
 require 'date'
 require 'bcrypt'
+require 'optparse'
+require 'fileutils'
 
 $stdout.sync = true
 
-puts
-puts "This script will generate a new user and account."
-puts "Enter the required information. Just hit enter to accept the default value."
-puts
+puts ""
 
-print "Person full name: "
-STDOUT.flush
-person_name = STDIN.gets.chomp
+options = {
+  base_uri: "http://ext.data.gift",
+  salt: ""
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: mu script generate-account [options]"
 
-print "Account name: "
-STDOUT.flush
-account_name = STDIN.gets.chomp
+  opts.on("-n", "--name NAME", "Person full name") do |name|
+    options[:name] = name
+  end
 
-print "Password: "
-STDOUT.flush
-password = STDIN.gets.chomp
+  opts.on("-a", "--account ACCOUNT", "Account name") do |account|
+    options[:account] = account
+  end
 
-default_salt = ""
-print "Application salt (default: none): "
-STDOUT.flush
-application_salt = STDIN.gets.chomp
-application_salt = default_salt if application_salt.empty?
+  opts.on("-p", "--password PASSWORD", "Password") do |password|
+    options[:password] = password
+  end
 
-default_domain = "http://ext.data.gift"
-print "Resource domain (default: #{default_domain}): "
-STDOUT.flush
-domain = STDIN.gets.chomp
-domain = default_domain if domain.empty?
+  opts.on("--base-uri BASE_URI", "Base URI for resources (default: http://ext.data.gift)") do |base_uri|
+    options[:base_uri] = base_uri
+  end
+
+  opts.on("--salt APPLICATION_SALT", "Application salt (optional)") do |salt|
+    options[:salt] = salt
+  end
+end.parse!
+
+[:name, :account, :password].each do |key|
+  if options[key].nil? || options[key].empty?
+    puts "Failed to execute script:"
+    puts "  --#{key} is a required option"
+    exit(1)
+  end
+end
 
 account_salt = SecureRandom.hex
 person_uuid = SecureRandom.uuid
 account_uuid = SecureRandom.uuid
-hashed_password = BCrypt::Password.create(password + application_salt + account_salt)
+hashed_password = BCrypt::Password.create(options[:password] + options[:salt] + account_salt)
 now = DateTime.now.xmlschema
 
 query = %(
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX people: <#{domain}/people/>
-PREFIX accounts: <#{domain}/accounts/>
+PREFIX people: <#{options[:base_uri]}/people/>
+PREFIX accounts: <#{options[:base_uri]}/accounts/>
 PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX mu:      <http://mu.semte.ch/vocabularies/core/>
+PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 PREFIX account: <http://mu.semte.ch/vocabularies/account/>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
 INSERT DATA {
    GRAPH <http://mu.semte.ch/graphs/users> {
      people:#{person_uuid} a foaf:Person ;
-                   foaf:name "#{person_name}" ;
+                   foaf:name "#{options[:name]}" ;
                    foaf:account accounts:#{account_uuid} ;
                    mu:uuid "#{person_uuid}" ;
                    dcterms:created "#{now}"^^xsd:dateTime ;
                    dcterms:modified "#{now}"^^xsd:dateTime .
      accounts:#{account_uuid} a foaf:OnlineAccount ;
-                   foaf:accountName "#{account_name}" ;
+                   foaf:accountName "#{options[:account]}" ;
                    mu:uuid "#{account_uuid}" ;
                    account:password """#{hashed_password}""" ;
                    account:salt "#{account_salt}" ;
@@ -70,9 +81,10 @@ INSERT DATA {
 }
 )
 
+migrations_folder = "/data/app/config/migrations"
+FileUtils.mkdir_p(migrations_folder)
 timestamp = DateTime.now.strftime("%Y%m%d%H%M%S")
-filename = "#{timestamp}-create-user-#{account_name.gsub(/\s|\./, "-")}.sparql"
-puts
-puts "Copy the following contents to ./config/migrations/#{filename}"
-puts
-puts query
+filename = "#{timestamp}-create-user-#{options[:account].gsub(/\s|\./, "-")}.sparql"
+File.write("#{migrations_folder}/#{filename}", query, encoding: "utf-8")
+
+puts "Migration written to ./config/migrations/#{filename}"
